256968d3244431887239c0e7b26a7100ed0537e5
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-13 21:09:26 -0600

    logging results succesfully

--- run.js
+++ run.js
@@ -7,7 +7,7 @@ var BluePen = require('./writing-utensils').BluePen;
 
 
 var bic = new Pen(10);
-// console.log(bic);
+console.log(bic);
 bic.write('hello')
 bic.write('bye')
 
@@ -17,11 +17,11 @@ fountainPen.refill((10));
 console.log(fountainPen.inkLevel);
 
 var marker = new Marker(20);
-// console.log(marker);
-// console.log(Marker.prototype);
+console.log(marker);
+console.log(Marker.prototype);
 
 var pencil = new Pencil(25);
-// console.log(pencil);
+console.log(pencil);
 
 var pointPen = new PointPen(30)
 console.log(pointPen.inkLevel);
