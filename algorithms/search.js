04dce4a8d4246bc406254aedf03fbf8bcc89d7aa
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-01 14:40:57 -0600

    search algorithm finished

--- algorithms/search.js
+++ algorithms/search.js
@@ -1,4 +1,13 @@
-function Search (array) {
-  var array = ['Sam', 'Lucas', 'Thomas', 'Katalin', 'Linda'];
+var array = ['Sam', 'Lucas', 'Thomas', 'Katalin', 'Linda'];
+var found_names = [];
+
+function Search (sub) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].toLowerCase().indexOf(sub) > -1){
+       found_names.push(array[i]);
+    }
+  }
+  return found_names;
 }
-var sub = 'as'
+
+console.log(Search("sam"));
