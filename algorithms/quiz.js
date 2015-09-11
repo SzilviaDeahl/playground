f7a05d4e3572b8c8eea3b2e0170a88c581bcf05e
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-11 14:01:53 -0600

    roman numerals recursion

--- algorithms/quiz.js
+++ algorithms/quiz.js
@@ -8,3 +8,34 @@ function printWord(word) {
 }
 
 printWord('elkelkaposztasitottalanitottatok');
+
+var romanNumerals = {
+ 'M': 1000,
+ 'D': 500,
+ 'C': 100,
+ 'L': 50,
+ 'X': 10,
+ 'V': 5,
+ 'I': 1
+};
+
+
+total = 0 ;
+function romans(string){
+  if (string === ''){
+    return total;
+  } else {
+    total += romanNumerals[string[0]];
+    return romans(string.slice(1)) ;
+  }
+}
+//
+// XVIII
+// VIII
+// III
+// II
+// I
+// ""
+
+result = romans("XVIII");
+console.log(result);
