c3108c3219939790396586b8b27f86ec57c9e7b8
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-17 09:38:25 -0600

    times overlapping working

--- algorithms/timeOverlap.js
+++ algorithms/timeOverlap.js
@@ -1,14 +1,44 @@
 function isOverlapped (timespanA, timespanB) {
-    //return true if overlapping, false otherwise
+  if(timespanA.end > timespanB.start && timespanA.end < timespanB.end){
+    console.log('true');
+  } else if (timespanA.start > timespanB.start && timespanA.end < timespanB.end || timespanA.end >timespanB.end){
+    console.log('true');
+  } else {
+    console.log('false');
+  }
 }
-var christmasTimespan = {
-    start: new Date("December 25, 2015"),
-    end: new Date("December 26, 2015")
+
+
+var timespans = {
+	christmas: {
+		start: new Date("December 25, 2015"),
+		end: new Date("December 26, 2015")
+	},
+	halloween: {
+		start: new Date("October 31, 2015"),
+		end: new Date("November 1, 2015")
+	},
+	thisMillenium: {
+		start: new Date("January 1, 2000"),
+		end: new Date("January 1, 3000")
+	},
+	event1: {
+		start: new Date("March 20, 2015"),
+		end: new Date("March 21, 2015")
+	},
+	event2: {
+		start: new Date("March 21, 2015"),
+		end: new Date("March 22, 2015")
+	},
+	event3: {
+		start: new Date("March 19, 2015"),
+		end: new Date("March 20, 2015")
+	},
+	event4: {
+		start: new Date("March 19, 2015"),
+		end: new Date("March 22, 2015")
+	}
 };
 
-var winterBreak = {
-  start: new Date('December 21, 2015'),
-  end: new Date('December 31, 2015')
-};
 
-console.log(isOverlapped);
+isOverlapped(timespans.halloween, timespans.christmas);
