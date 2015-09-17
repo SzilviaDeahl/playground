baf8839867edbed6af5cc48ff6fce967743e9d52
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-17 09:24:38 -0600

    timeOverlap algorithm

---
+++ algorithms/timeOverlap.js
@@ -0,0 +1,14 @@
+function isOverlapped (timespanA, timespanB) {
+    //return true if overlapping, false otherwise
+}
+var christmasTimespan = {
+    start: new Date("December 25, 2015"),
+    end: new Date("December 26, 2015")
+};
+
+var winterBreak = {
+  start: new Date('December 21, 2015'),
+  end: new Date('December 31, 2015')
+};
+
+console.log(isOverlapped);
